9a86fd47e09e89d4c63d5254c20ac679b444692c
Joseph Lee <jhjlee702@gmail.com>
2023-09-26 09:37:12 -0600

    Add #add_bid method to class and test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,9 +1,17 @@
-require './spec_helper'
+require './spec/spec_helper'
 
 RSpec.describe Item do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+
+    @attendee1 = attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+
+    @auction = Auction.new
   end
 
   describe '#initialize' do
@@ -12,6 +20,20 @@ RSpec.describe Item do
       expect(@item2).to be_a(Item)
       expect(@item1.name).to eq("Chalkware Piggy Bank")
       expect(@item2.name).to eq('Bamboo Picture Frame')
+    end
+  end
+
+  describe '#add_bid' do
+    it 'adds an attendees bid to the item' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      expect(@item1.bids).to eq({})
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
     end
   end
 end
