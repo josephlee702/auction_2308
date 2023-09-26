c9fe34f1bb7de55951878de5563a916cb16b445b
Joseph Lee <jhjlee702@gmail.com>
2023-09-26 10:15:12 -0600

    Write unpopular and potential_revenue methods

--- lib/auction.rb
+++ lib/auction.rb
@@ -10,4 +10,25 @@ class Auction
     @items << item
     @item_names << item.name
   end
+
+  def unpopular_items
+    no_bid_items = []
+    @items.each do |item|
+      if item.bids == {}
+        no_bid_items << item
+      end
+    end
+    no_bid_items
+  end
+
+  def potential_revenue
+    #need to take the max bid on each item and add them together
+    pot_rev = 0
+    @items.each do |item|
+      if item.bids != {}
+        pot_rev += item.current_high_bid
+      end
+    end
+    pot_rev
+  end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -30,4 +30,28 @@ RSpec.describe Auction do
       expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
   end
+
+  describe '#unpopular_items and potential revenue' do
+    it 'gives items that have no bids on them and potential revenue of the auction, respectively' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.unpopular_items).to eq([@item2, @item5])
+      expect(@auction.potential_revenue).to eq(87)
+      #extra tests
+      @item1.add_bid(@attendee5, 100)
+      expect(@item1.current_high_bid).to eq(100)
+      expect(@auction.potential_revenue).to eq(165)
+      @item5.add_bid(@attendee2, 35)
+      expect(@auction.unpopular_items).to eq([@item2])
+      expect(@auction.potential_revenue).to eq(200)
+    end
+  end
 end
